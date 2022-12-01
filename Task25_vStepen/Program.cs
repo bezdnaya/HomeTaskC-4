e522b84cf0e35db01df3739467b5ab8bb27a9ce2
Irina <bezdnaya@mail.ru>
2022-12-01 12:38:59 +0200

    Задача 25 Степень в цикле

---
+++ Task25_vStepen/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine($"Введите числа A и В =");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+if (B == 0)
+    Console.WriteLine($"Число А в степени {B} = 1");
+else 
+{
+    for (int i = 1; i < B; i++)
+    A = A*A;
+}
+Console.WriteLine($"Число А в степени {B} = {A}");
