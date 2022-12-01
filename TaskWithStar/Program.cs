d9f8b3b97710bbb1ec8f36f1ff0078130363bbca
Irina <bezdnaya@mail.ru>
2022-12-01 15:42:16 +0200

    Решение, которое не удовлетворяет условию что все числа разные

---
+++ TaskWithStar/Program.cs
@@ -0,0 +1,13 @@
+﻿// найдите х5, при котором х1^5+x2^5+x3^5+x4^5 = x5^5
+
+for (int i = 4; i < 201; i++)
+{
+    double result = 276 + Math.Pow(i, 5);
+    //Console.WriteLine(i);
+    //Console.WriteLine(result);
+    double x5 = Math.Pow(result, 0.2);
+    //Console.WriteLine(x5);
+    if (Math.Round(x5, 6)*1000000%1000000 == 0)
+        Console.WriteLine($"x1 = 1, x2 = 2, x3 = 3, x4 = {i}, x5 = {Math.Round(x5,4)}");
+        Console.WriteLine();
+}
